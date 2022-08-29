c83d20136fcf05bf47065047e9fb6988276d96be
nosamaj <70916594+nosamaj@users.noreply.github.com>
2022-08-29 19:11:31 +0100

    refactoring to multi page app to add functions

--- pages/1_🌍_Station_Mapping.py
+++ pages/1_🌍_Station_Mapping.py
@@ -3,9 +3,7 @@ import streamlit as st
 import ea_api_request
 #import utils.fetcharchive as fetcharchive
 #import utils.create_db_tables as create_db_tables
-import pydeck as pdk
-
-
+import leafmap.foliumap as leafmap
 
 st.title('EA Open Data Viewer')
 
@@ -17,36 +15,6 @@ st.markdown('''This application will allow the user to explore the EA flooding A
 The options available are to see a map of the available sites, graph data for comparision and explore a statistical analaysis of data.''')
 
 df_stations = ea_api_request.get_stations()
-
-
-    
-# r = pdk.Deck(
-#      map_style="light",
-#      initial_view_state=pdk.ViewState(
-#          latitude= 53.2,
-#          longitude=-3,
-#          zoom=5,
-#          pitch=0,
-#      ),
-#      layers=[
-#          pdk.Layer(
-#              'ScatterplotLayer',
-#              data=df_stations,
-#              get_position='[long, lat]',
-#              get_color='[200, 30, 0, 160]',
-#              get_radius=200,
-#          )
-#      ]
-#  )
-
-#r.to_html("test.html", open_browser=True, notebook_display=False)
-
-#st.pydeck_chart(r)
-
-import leafmap.foliumap as leafmap
-
-
-
 
 st.title("Marker Cluster")
 
