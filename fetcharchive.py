20259d13712fdd2151d4157d7bc1d1c83b397987
James Mason <nosamaj@gmail.com>
2022-11-13 20:48:07 +0000

    added ML ML_Experiment

--- fetcharchive.py
+++ fetcharchive.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pandas.io.common
 
 
-start = datetime.datetime.strptime("01-09-2021", "%d-%m-%Y")
+start = datetime.datetime.strptime("01-05-2022", "%d-%m-%Y")
 date_generated = pd.date_range(start, periods=375)
 print(date_generated.strftime("%Y-%m-%d"))
 
@@ -17,7 +17,7 @@ try:
             date = d.strftime("%Y-%m-%d")
             print(date)
             df = pd.read_csv(f"https://environment.data.gov.uk/flood-monitoring/archive/readings-{date}.csv")
-            df.to_csv(f"/home/nosamaj/EAData/{date}")
+            df.to_csv(f"/home/nosamaj/EAdata/{date}")
         except Exception:
             pass
         #df.to_csv(f"./EAdata/{date}")
